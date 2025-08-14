7e62a6ae4c7ace574c80b1177620d0bfe428c982
martinvonk <vonk.mart@gmail.com>
2025-08-14 12:18:02 +0200

    update model settings

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,15 @@
 from pathlib import Path
 
+from pandas import Timestamp
 from shapely import LineString
 
+paths = {
+    "models": Path(__file__).parent.parent / "modeldata/models",
+    "downloads": Path(__file__).parent.parent / "modeldata/downloads",
+}
+
 model_name = "Haamstede"
-extent = ([35_000.0, 48_000.0, 410_000.0, 419_000.0],)
+extent = [35_000.0, 46_000.0, 409_000.0, 419_000.0]
 cross_sections = (
     {
         "A": LineString([(35632.0, 411759.0), (47848.0, 416205.0)]),
@@ -11,9 +17,8 @@ cross_sections = (
         "C": LineString([(37030.0, 412170.0), (39150.0, 413160.0)]),
     },
 )
-tmin = "2013"
-tmax = "2024"
-paths = {
-    "models": Path(__file__).parent.parent / "modeldata/models",
-    "downloads": Path(__file__).parent.parent / "modeldata/downloads",
-}
+tmin = Timestamp("2013-01-01")
+tmax = Timestamp("2024-12-31")
+delr = 500.0
+delc = 500.0
+steady = True
